078307a85d027063465ab51199b5fc02f5f32f19
devops-user22 <102788347+devops-user22@users.noreply.github.com>
2024-11-04 16:08:37 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,8 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+  }
   cloud { 
     
     organization = "Integrant" 
@@ -12,7 +14,6 @@ terraform {
       name = "terraform-demo" 
     } 
   } 
-  }
 }
 
 provider "aws" {
