1b5f4c2dafd2be1b181fa415fbc74f7abe4a01e7
devops-user22 <102788347+devops-user22@users.noreply.github.com>
2024-11-04 16:37:39 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,3 @@
-
-
-
-terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,27 +5,14 @@ terraform {
     }
 
   }
+  cloud "terraform" { 
+    
+    organization = "Integrant" 
+    workspaces { 
+      name = "terraform-demo" 
+    } 
+  } 
 
-
-
-  backend "remote" {
-    organization = "Integrant"
-    workspaces {
-      name = "terraform-demo"
-    }
-  }
-}
-
-# Load the appropriate variables file based on the workspace
-data "terraform_remote_state" "variables" {
-  backend = "remote"
-  config = {
-    organization = "Integrant"
-    workspaces = {
-      name = terraform-demo
-    }
-  }
-}
 provider "aws" {
     access_key = "AKIAVYV5Z3DTRUDTV4QU"
     secret_key = "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"
