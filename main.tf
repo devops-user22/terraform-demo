dd4306fcc7d7ea6aa949cd7ab22c2e3a89b43272
devops-user22 <102788347+devops-user22@users.noreply.github.com>
2024-11-04 16:27:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,16 +9,6 @@ locals {
 }
 
 
-# Load the appropriate variables file based on the workspace
-data "terraform_remote_state" "variables" {
-  backend = "remote"
-  config = {
-    organization = "your-organization"
-    workspaces = {
-      name = local.variables_file
-    }
-  }
-}
 resource "aws_s3_bucket" "neuro-ace-services" {
   bucket = var.buketname
 }
