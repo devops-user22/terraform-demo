0ca1dc110bd2abb53e57e25d87e0503b0242379a
devops-user22 <102788347+devops-user22@users.noreply.github.com>
2024-11-04 16:41:27 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,25 @@
 terraform {
-required_providers {
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-
   }
+}
+
+provider "aws" {
+  region  = var.aws_region
+      access_key = "AKIAVYV5Z3DTRUDTV4QU"
+    secret_key = "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"
+}
+terraform { 
   cloud { 
     
     organization = "Integrant" 
+
     workspaces { 
       name = "terraform-demo" 
     } 
   } 
 }
-provider "aws" {
-    access_key = "AKIAVYV5Z3DTRUDTV4QU"
-    secret_key = "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"
-}
-
 
