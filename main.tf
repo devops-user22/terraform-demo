3c27d33644c3894048709c9c573248ca6007953a
devops-user22 <102788347+devops-user22@users.noreply.github.com>
2024-11-04 17:19:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+
+resource "tfe_workspace" "workspace" {
+  name                = "stage"
+  organization        = "Integrant"
+  auto_apply          = false
+  global_remote_state = true
+  queue_all_runs      = false
+  terraform_version   = "v1.9.8"
+  working_directory   = "terraform"
+  trigger_prefixes    = ["terraform/*"]
+  project_id          = "ws-vq1mQVprPoHYP4EV"
+ 
+  vcs_repo {
+    identifier     = "https://github.com/devops-user22/terraform-demo"
+    branch         = "main"
+    oauth_token_id = "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"
+  }
+}
+
+
+
 variable "bucketname" {
   description = "Name of the S3 bucket"
   type        = string
