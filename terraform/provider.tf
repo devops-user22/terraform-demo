be9d83eea93ccb13243de5e9ee12df84181a52a7
devops-user22 <102788347+devops-user22@users.noreply.github.com>
2024-11-05 17:48:57 +0200

    add

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,18 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
-provider "aws" {
+terraform { 
+  provider "aws" {
       region  = "us-east-2"
       access_key = ""
     secret_key = ""
-}
-terraform { 
+        }
   cloud { 
     
     organization = "Integrant" 
